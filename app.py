a01909ab4af29ddfa69683a41d081f268d2bb6f5
AdaelRuiz <resteban.ruiz1983@gmail.com>
2018-01-08 00:04:21 -0600

    Actualizando las route

--- app.py
+++ app.py
@@ -8,6 +8,51 @@ def index():
 	context = {"page_title": "Moonlight Store", "current_year": date.today().year}
 	return render_template('index.html')
 
+@app.route('/')
+def about_us():
+	context = {"page_title": "Moonlight Store", "current_year": date.today().year}
+	return render_template('about_us.html')
+
+@app.route('/')
+def cart():
+	context = {"page_title": "Moonlight Store", "current_year": date.today().year}
+	return render_template('cart.html')
+
+@app.route('/')
+def compair():
+	context = {"page_title": "Moonlight Store", "current_year": date.today().year}
+	return render_template('compair.html')
+
+@app.route('/')
+def contact():
+	context = {"page_title": "Moonlight Store", "current_year": date.today().year}
+	return render_template('contact.html')
+
+@app.route('/')
+def footer():
+	context = {"page_title": "Moonlight Store", "current_year": date.today().year}
+	return render_template('footer.html')
+
+@app.route('/')
+def forget_password():
+	context = {"page_title": "Moonlight Store", "current_year": date.today().year}
+	return render_template('forget_password.html')
+
+@app.route('/')
+def four_col():
+	context = {"page_title": "Moonlight Store", "current_year": date.today().year}
+	return render_template('four-col.html')
+
+@app.route('/')
+def general():
+	context = {"page_title": "Moonlight Store", "current_year": date.today().year}
+	return render_template('general.html')
+
+
+
+
+
+
 
 
 
